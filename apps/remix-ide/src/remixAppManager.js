c8be4d7a1065ac836fbc1db21ef8f0a9551a8721
yongen.loong <yongen.loong@aelf.io>
2024-09-09 04:00:33 +0800

    update plugins directory repo

--- apps/remix-ide/src/remixAppManager.js
+++ apps/remix-ide/src/remixAppManager.js
@@ -160,7 +160,7 @@ export class RemixAppManager extends PluginManager {
   constructor() {
     super()
     this.event = new EventEmitter()
-    this.pluginsDirectory = 'https://raw.githubusercontent.com/ethereum/remix-plugins-directory/master/build/metadata.json'
+    this.pluginsDirectory = 'https://raw.githubusercontent.com/yongenaelf/remix-plugins-directory/master/build/metadata.json'
     this.pluginLoader = new PluginLoader()
     if (Registry.getInstance().get('platform').api.isDesktop()) {
       requiredModules = [...requiredModules, 'fs', 'electronTemplates', 'isogit', 'remix-templates', 'electronconfig', 'xterm', 'compilerloader', 'ripgrep']
