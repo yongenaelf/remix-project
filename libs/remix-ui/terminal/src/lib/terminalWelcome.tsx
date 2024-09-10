c99ce0f036b4203a0d7dbcaad48124975396d13e
yongen.loong <yongen.loong@aelf.io>
2024-09-10 16:55:42 +0800

    hide terminal welcome text

--- libs/remix-ui/terminal/src/lib/terminalWelcome.tsx
+++ libs/remix-ui/terminal/src/lib/terminalWelcome.tsx
@@ -6,7 +6,8 @@ const TerminalWelcomeMessage = ({ packageJson, storage }) => {
     <div className="remix_ui_terminal_block px-4 " data-id="block_null">
       <div className="remix_ui_terminal_welcome">
         {' '}
-        <FormattedMessage id="terminal.welcomeText1" /> Remix {packageJson}{' '}
+        {/* <FormattedMessage id="terminal.welcomeText1" /> Remix {packageJson}{' '} */}
+        <FormattedMessage id="terminal.welcomeText1" /> aelf Studio{' '}
       </div>
       <br />
       <div className="">
@@ -20,7 +21,7 @@ const TerminalWelcomeMessage = ({ packageJson, storage }) => {
         <li key="details-and-debug">
           <FormattedMessage id="terminal.welcomeText4" />.
         </li>
-        <li key="run-javascript">
+        {/* <li key="run-javascript">
           <FormattedMessage id="terminal.welcomeText5" />:
           <br />
           <i>
@@ -37,10 +38,10 @@ const TerminalWelcomeMessage = ({ packageJson, storage }) => {
             {' '}
             - <FormattedMessage id="terminal.welcomeText8" />{' '}
           </i>
-        </li>
+        </li> */}
       </ul>
 
-      <div>
+      {/* <div>
         <FormattedMessage id="terminal.welcomeText9" />:
       </div>
       <ul className="ml-0 mr-4">
@@ -54,13 +55,13 @@ const TerminalWelcomeMessage = ({ packageJson, storage }) => {
             ethers.js
           </a>{' '}
         </li>
-        {/* <li key="sol-gpt">
+        <li key="sol-gpt">
           sol-gpt <i>&lt;your Solidity question here&gt;</i> {' '}
-        </li> */}
+        </li>
       </ul>
       <div>
         <FormattedMessage id="terminal.welcomeText10" />.
-      </div>
+      </div> */}
     </div>
   )
 }
