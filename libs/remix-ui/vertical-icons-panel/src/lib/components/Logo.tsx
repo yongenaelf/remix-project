421c822c0051bbbe0cbd567278613ba5a719f56c
yongen.loong <yongen.loong@aelf.io>
2024-09-10 14:50:44 +0800

    update logo

---
+++ libs/remix-ui/vertical-icons-panel/src/lib/components/Logo.tsx
@@ -0,0 +1,387 @@
+import * as React from "react"
+const StudioLogo = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    id="Layer_2"
+    viewBox="0 0 126.99 127"
+    {...props}
+  >
+    <defs>
+      <clipPath id="clippath">
+        <path d="M1 1h125v125H1z" className="cls-1" />
+      </clipPath>
+      <clipPath id="clippath-1">
+        <rect
+          width={126.88}
+          height={126.88}
+          className="cls-1"
+          rx={36}
+          ry={36}
+        />
+      </clipPath>
+      <clipPath id="clippath-2">
+        <rect
+          width={124.48}
+          height={124.48}
+          x={1.2}
+          y={1.2}
+          className="cls-1"
+          rx={36}
+          ry={36}
+        />
+      </clipPath>
+      <clipPath id="clippath-3">
+        <path
+          d="M31.75 0h63.5c1.04 0 2.08.05 3.11.16 1.04.1 2.06.26 3.08.46s2.03.45 3.02.76 1.97.65 2.93 1.05c.96.4 1.9.84 2.82 1.33a32.489 32.489 0 0 1 5.18 3.45c.8.66 1.57 1.36 2.31 2.09s1.43 1.51 2.09 2.31c.66.81 1.28 1.64 1.85 2.51.58.86 1.11 1.76 1.6 2.67.49.92.94 1.85 1.33 2.82.4.96.75 1.94 1.05 2.93.3.99.55 2 .76 3.02s.35 2.05.46 3.08c.1 1.04.15 2.07.15 3.11v63.5c0 1.04-.05 2.08-.15 3.11-.1 1.04-.26 2.06-.46 3.08s-.45 2.03-.76 3.02c-.3.99-.65 1.97-1.05 2.93-.4.96-.84 1.9-1.33 2.82-.49.92-1.02 1.81-1.6 2.67-.58.86-1.2 1.7-1.85 2.51-.66.8-1.36 1.57-2.09 2.31s-1.51 1.43-2.31 2.09c-.81.66-1.64 1.28-2.51 1.85s-1.76 1.11-2.67 1.6c-.92.49-1.85.93-2.82 1.33-.96.4-1.94.75-2.93 1.05-.99.3-2.01.55-3.02.76-1.02.2-2.05.36-3.08.46-1.04.1-2.07.15-3.11.15h-63.5c-1.04 0-2.08-.05-3.11-.15-1.04-.1-2.06-.26-3.08-.46s-2.03-.45-3.02-.76a31.377 31.377 0 0 1-5.75-2.38c-.92-.49-1.81-1.03-2.67-1.6-.86-.58-1.7-1.2-2.51-1.85-.8-.66-1.57-1.36-2.31-2.09s-1.43-1.51-2.09-2.31c-.66-.81-1.28-1.64-1.85-2.51a31.405 31.405 0 0 1-2.93-5.49c-.4-.96-.74-1.94-1.05-2.93-.3-.99-.56-2-.76-3.02s-.35-2.05-.45-3.08c-.1-1.04-.16-2.07-.16-3.11v-63.5c0-1.04.05-2.07.16-3.11.1-1.04.25-2.06.45-3.08s.46-2.03.76-3.02c.3-.99.65-1.97 1.05-2.93.4-.96.84-1.9 1.33-2.82a32.489 32.489 0 0 1 3.45-5.18c.66-.8 1.36-1.57 2.09-2.31s1.51-1.43 2.31-2.09c.8-.66 1.64-1.28 2.51-1.85.86-.58 1.76-1.11 2.67-1.6.92-.49 1.85-.93 2.81-1.33s1.94-.75 2.94-1.05c.99-.3 2-.55 3.02-.76s2.05-.36 3.08-.46C29.67.06 30.71 0 31.75 0Z"
+          className="cls-1"
+        />
+      </clipPath>
+      <clipPath id="clippath-4">
+        <path d="M26.84 44.94h22.67v37.33H26.84z" className="cls-1" />
+      </clipPath>
+      <clipPath id="clippath-5">
+        <rect
+          width={126.88}
+          height={126.88}
+          className="cls-1"
+          rx={36}
+          ry={36}
+        />
+      </clipPath>
+      <clipPath id="clippath-6">
+        <rect
+          width={124.48}
+          height={124.48}
+          x={1.2}
+          y={1.2}
+          className="cls-1"
+          rx={36}
+          ry={36}
+        />
+      </clipPath>
+      <clipPath id="clippath-7">
+        <path d="M77.51 44.94h22.67v37.33H77.51z" className="cls-1" />
+      </clipPath>
+      <clipPath id="clippath-8">
+        <rect
+          width={126.88}
+          height={126.88}
+          className="cls-1"
+          rx={36}
+          ry={36}
+        />
+      </clipPath>
+      <clipPath id="clippath-9">
+        <rect
+          width={124.48}
+          height={124.48}
+          x={1.2}
+          y={1.2}
+          className="cls-1"
+          rx={36}
+          ry={36}
+        />
+      </clipPath>
+      <clipPath id="clippath-10">
+        <path d="M52.18 35.61h22.67v56H52.18z" className="cls-1" />
+      </clipPath>
+      <clipPath id="clippath-11">
+        <rect
+          width={126.88}
+          height={126.88}
+          className="cls-1"
+          rx={36}
+          ry={36}
+        />
+      </clipPath>
+      <clipPath id="clippath-12">
+        <rect
+          width={124.48}
+          height={124.48}
+          x={1.2}
+          y={1.2}
+          className="cls-1"
+          rx={36}
+          ry={36}
+        />
+      </clipPath>
+      <clipPath id="clippath-13">
+        <path d="M0 0h126.99v127H0z" className="cls-1" />
+      </clipPath>
+      <clipPath id="clippath-14">
+        <rect
+          width={126.88}
+          height={126.88}
+          className="cls-1"
+          rx={36}
+          ry={36}
+        />
+      </clipPath>
+      <clipPath id="clippath-15">
+        <path
+          d="M0 36v54.88c0 19.88 16.12 35.99 36 35.99h54.88c19.89 0 36-16.11 36-35.99V36c0-19.88-16.12-36-36-36H36C16.12 0 0 16.12 0 36Z"
+          className="cls-1"
+        />
+      </clipPath>
+      <linearGradient
+        id="linear-gradient"
+        x1={-111.64}
+        x2={-72.44}
+        y1={398.93}
+        y2={359.72}
+        gradientTransform="matrix(3.17 0 0 -3.17 355.66 1267.8)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#9b8dff" />
+        <stop offset={0.06} stopColor="#9b8dff" />
+        <stop offset={0.09} stopColor="#9b8dff" />
+        <stop offset={0.11} stopColor="#9b8dff" />
+        <stop offset={0.11} stopColor="#9a8cff" />
+        <stop offset={0.12} stopColor="#9a8cff" />
+        <stop offset={0.12} stopColor="#998cff" />
+        <stop offset={0.12} stopColor="#998bff" />
+        <stop offset={0.13} stopColor="#988bff" />
+        <stop offset={0.13} stopColor="#978bff" />
+        <stop offset={0.14} stopColor="#978aff" />
+        <stop offset={0.14} stopColor="#968aff" />
+        <stop offset={0.14} stopColor="#9689ff" />
+        <stop offset={0.15} stopColor="#9589ff" />
+        <stop offset={0.15} stopColor="#9589ff" />
+        <stop offset={0.16} stopColor="#9488ff" />
+        <stop offset={0.16} stopColor="#9388fe" />
+        <stop offset={0.16} stopColor="#9388fe" />
+        <stop offset={0.17} stopColor="#9287fe" />
+        <stop offset={0.17} stopColor="#9287fe" />
+        <stop offset={0.18} stopColor="#9186fe" />
+        <stop offset={0.18} stopColor="#9186fe" />
+        <stop offset={0.18} stopColor="#9086fe" />
+        <stop offset={0.19} stopColor="#8f85fe" />
+        <stop offset={0.19} stopColor="#8f85fe" />
+        <stop offset={0.2} stopColor="#8e85fe" />
+        <stop offset={0.2} stopColor="#8e84fe" />
+        <stop offset={0.2} stopColor="#8d84fe" />
+        <stop offset={0.21} stopColor="#8d83fe" />
+        <stop offset={0.21} stopColor="#8c83fe" />
+        <stop offset={0.21} stopColor="#8b83fe" />
+        <stop offset={0.22} stopColor="#8b82fe" />
+        <stop offset={0.22} stopColor="#8a82fe" />
+        <stop offset={0.23} stopColor="#8a82fe" />
+        <stop offset={0.23} stopColor="#8981fe" />
+        <stop offset={0.23} stopColor="#8981fe" />
+        <stop offset={0.24} stopColor="#8880fe" />
+        <stop offset={0.24} stopColor="#8780fe" />
+        <stop offset={0.25} stopColor="#8780fe" />
+        <stop offset={0.25} stopColor="#867ffe" />
+        <stop offset={0.25} stopColor="#867ffe" />
+        <stop offset={0.26} stopColor="#857ffe" />
+        <stop offset={0.26} stopColor="#857efd" />
+        <stop offset={0.27} stopColor="#847efd" />
+        <stop offset={0.27} stopColor="#837dfd" />
+        <stop offset={0.27} stopColor="#837dfd" />
+        <stop offset={0.28} stopColor="#827dfd" />
+        <stop offset={0.28} stopColor="#827cfd" />
+        <stop offset={0.29} stopColor="#817cfd" />
+        <stop offset={0.29} stopColor="#817cfd" />
+        <stop offset={0.29} stopColor="#807bfd" />
+        <stop offset={0.3} stopColor="#7f7bfd" />
+        <stop offset={0.3} stopColor="#7f7afd" />
+        <stop offset={0.3} stopColor="#7e7afd" />
+        <stop offset={0.31} stopColor="#7e7afd" />
+        <stop offset={0.31} stopColor="#7d79fd" />
+        <stop offset={0.32} stopColor="#7d79fd" />
+        <stop offset={0.33} stopColor="#7c78fd" />
+        <stop offset={0.34} stopColor="#7b78fd" />
+        <stop offset={0.34} stopColor="#7a77fd" />
+        <stop offset={0.35} stopColor="#7976fd" />
+        <stop offset={0.36} stopColor="#7876fd" />
+        <stop offset={0.37} stopColor="#7775fd" />
+        <stop offset={0.38} stopColor="#7674fc" />
+        <stop offset={0.38} stopColor="#7574fc" />
+        <stop offset={0.39} stopColor="#7473fc" />
+        <stop offset={0.4} stopColor="#7373fc" />
+        <stop offset={0.41} stopColor="#7272fc" />
+        <stop offset={0.41} stopColor="#7171fc" />
+        <stop offset={0.42} stopColor="#7071fc" />
+        <stop offset={0.43} stopColor="#6f70fc" />
+        <stop offset={0.44} stopColor="#6e6ffc" />
+        <stop offset={0.45} stopColor="#6e6ffc" />
+        <stop offset={0.45} stopColor="#6d6efc" />
+        <stop offset={0.46} stopColor="#6c6dfc" />
+        <stop offset={0.47} stopColor="#6b6dfc" />
+        <stop offset={0.48} stopColor="#6a6cfc" />
+        <stop offset={0.48} stopColor="#696cfc" />
+        <stop offset={0.49} stopColor="#686bfc" />
+        <stop offset={0.5} stopColor="#676afb" />
+        <stop offset={0.5} stopColor="#666afb" />
+        <stop offset={0.51} stopColor="#666afb" />
+        <stop offset={0.52} stopColor="#6569fb" />
+        <stop offset={0.52} stopColor="#6468fb" />
+        <stop offset={0.53} stopColor="#6368fb" />
+        <stop offset={0.54} stopColor="#6267fb" />
+        <stop offset={0.55} stopColor="#6166fb" />
+        <stop offset={0.55} stopColor="#6066fb" />
+        <stop offset={0.56} stopColor="#5f65fb" />
+        <stop offset={0.57} stopColor="#5f65fb" />
+        <stop offset={0.58} stopColor="#5e64fb" />
+        <stop offset={0.59} stopColor="#5d64fb" />
+        <stop offset={0.59} stopColor="#5d63fb" />
+        <stop offset={0.6} stopColor="#5c63fb" />
+        <stop offset={0.61} stopColor="#5b62fa" />
+        <stop offset={0.62} stopColor="#5b62fa" />
+        <stop offset={0.62} stopColor="#5a61fa" />
+        <stop offset={0.63} stopColor="#5961fa" />
+        <stop offset={0.64} stopColor="#5960fa" />
+        <stop offset={0.65} stopColor="#5860fa" />
+        <stop offset={0.66} stopColor="#575ffa" />
+        <stop offset={0.66} stopColor="#575ffa" />
+        <stop offset={0.67} stopColor="#565efa" />
+        <stop offset={0.68} stopColor="#555efa" />
+        <stop offset={0.69} stopColor="#555dfa" />
+        <stop offset={0.7} stopColor="#545dfa" />
+        <stop offset={0.7} stopColor="#535dfa" />
+        <stop offset={0.71} stopColor="#535cfa" />
+        <stop offset={0.72} stopColor="#525cf9" />
+        <stop offset={0.73} stopColor="#515bf9" />
+        <stop offset={0.73} stopColor="#515bf9" />
+        <stop offset={0.74} stopColor="#505af9" />
+        <stop offset={0.75} stopColor="#4f5af9" />
+        <stop offset={0.76} stopColor="#4f59f9" />
+        <stop offset={0.77} stopColor="#4e59f9" />
+        <stop offset={0.77} stopColor="#4d58f9" />
+        <stop offset={0.78} stopColor="#4d58f9" />
+        <stop offset={0.8} stopColor="#4c58f9" />
+        <stop offset={0.81} stopColor="#4c57f9" />
+        <stop offset={0.83} stopColor="#4b57f9" />
+        <stop offset={0.84} stopColor="#4a56f9" />
+        <stop offset={0.86} stopColor="#4956f9" />
+        <stop offset={0.88} stopColor="#4955f9" />
+        <stop offset={0.89} stopColor="#4855f9" />
+        <stop offset={0.91} stopColor="#4755f9" />
+        <stop offset={0.94} stopColor="#4754f9" />
+        <stop offset={1} stopColor="#4654f9" />
+      </linearGradient>
+      <style>{".cls-1{fill:none}.cls-20{fill:rgba(255,255,255,.9)}"}</style>
+    </defs>
+    <g id="Layer_1-2" data-name="Layer_1">
+      <g
+        style={{
+          clipPath: "url(#clippath)",
+        }}
+      >
+        <g
+          style={{
+            clipPath: "url(#clippath-1)",
+          }}
+        >
+          <g
+            style={{
+              clipPath: "url(#clippath-2)",
+            }}
+          >
+            <g
+              style={{
+                clipPath: "url(#clippath-3)",
+              }}
+            >
+              <path
+                d="M1.2 1.2v124.48h124.48V1.2H1.2Z"
+                style={{
+                  fill: "url(#linear-gradient)",
+                }}
+              />
+            </g>
+          </g>
+        </g>
+      </g>
+      <g
+        style={{
+          clipPath: "url(#clippath-4)",
+        }}
+      >
+        <g
+          style={{
+            clipPath: "url(#clippath-5)",
+          }}
+        >
+          <path
+            d="M44.45 80.96c-1.32 0-2.44-.46-3.37-1.39l-12.7-12.7c-.45-.44-.79-.96-1.04-1.54-.24-.59-.36-1.19-.36-1.83s.13-1.24.36-1.82c.24-.58.59-1.1 1.04-1.54l12.7-12.7a4.776 4.776 0 0 1 5.19-1.03 4.723 4.723 0 0 1 2.58 2.57c.24.58.36 1.19.36 1.82s-.12 1.24-.36 1.82c-.24.58-.58 1.1-1.03 1.55l-9.33 9.33 9.33 9.33c.33.33.61.71.83 1.13.22.41.38.85.47 1.31.09.46.11.93.07 1.4s-.16.92-.34 1.36c-.18.43-.42.83-.72 1.2-.3.36-.65.68-1.04.94-.39.27-.81.46-1.27.6-.45.14-.91.2-1.38.2Z"
+            className="cls-20"
+            style={{
+              clipPath: "url(#clippath-6)",
+            }}
+          />
+        </g>
+      </g>
+      <g
+        style={{
+          clipPath: "url(#clippath-7)",
+        }}
+      >
+        <g
+          style={{
+            clipPath: "url(#clippath-8)",
+          }}
+        >
+          <path
+            d="M82.55 80.96c-.47 0-.93-.07-1.39-.2-.45-.14-.87-.33-1.26-.6-.4-.26-.74-.57-1.04-.94-.3-.36-.54-.77-.72-1.2-.18-.44-.3-.89-.34-1.36-.04-.47-.02-.93.07-1.4a4.843 4.843 0 0 1 1.3-2.44l9.33-9.33-9.33-9.33c-.45-.45-.79-.96-1.03-1.55s-.36-1.19-.36-1.82.12-1.24.36-1.82c.24-.58.59-1.09 1.04-1.54.44-.45.96-.79 1.54-1.03.58-.24 1.19-.36 1.82-.36s1.24.12 1.82.36c.58.24 1.1.59 1.55 1.03l12.7 12.7c.45.44.79.96 1.03 1.54a4.813 4.813 0 0 1 0 3.65c-.24.58-.58 1.1-1.03 1.54l-12.7 12.7c-.93.93-2.06 1.39-3.37 1.39Z"
+            className="cls-20"
+            style={{
+              clipPath: "url(#clippath-9)",
+            }}
+          />
+        </g>
+      </g>
+      <g
+        style={{
+          clipPath: "url(#clippath-10)",
+        }}
+      >
+        <g
+          style={{
+            clipPath: "url(#clippath-11)",
+          }}
+        >
+          <g
+            style={{
+              clipPath: "url(#clippath-12)",
+            }}
+          >
+            <path
+              d="M57.15 90.49c-.44 0-.88-.06-1.31-.18-.6-.18-1.16-.46-1.65-.85-.49-.4-.9-.86-1.2-1.42-.3-.55-.49-1.15-.57-1.77s-.02-1.24.15-1.85l12.7-44.45c.16-.62.44-1.18.83-1.69s.86-.92 1.42-1.23 1.15-.51 1.79-.59 1.26-.03 1.88.15c.61.17 1.17.46 1.67.86.5.4.9.89 1.21 1.45.3.56.49 1.16.55 1.8s0 1.26-.19 1.87l-12.7 44.45c-.15.49-.36.96-.66 1.39-.29.43-.64.8-1.06 1.1-.41.31-.86.55-1.35.72-.49.16-.99.24-1.51.24Z"
+              style={{
+                fill: "rgba(255,255,255,.5)",
+              }}
+            />
+          </g>
+        </g>
+      </g>
+      <g
+        style={{
+          clipPath: "url(#clippath-13)",
+        }}
+      >
+        <g
+          style={{
+            clipPath: "url(#clippath-14)",
+          }}
+        >
+          <g
+            style={{
+              clipPath: "url(#clippath-15)",
+            }}
+          >
+            <path
+              d="M0 36v54.88c0 19.88 16.12 36 36 36h54.88c19.89 0 36-16.11 36-35.99V36.01c0-19.89-16.12-36.01-36-36.01H36C16.12 0 0 16.12 0 36Z"
+              style={{
+                stroke: "#011853",
+                strokeWidth: 18,
+                fill: "none",
+              }}
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+)
+export default StudioLogo
