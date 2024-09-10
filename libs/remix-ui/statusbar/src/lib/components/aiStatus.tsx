7cda424c40a900d34392a84d76e40b6146a7ebb7
yongen.loong <yongen.loong@aelf.io>
2024-09-10 15:05:51 +0800

    rename to Copilot

--- libs/remix-ui/statusbar/src/lib/components/aiStatus.tsx
+++ libs/remix-ui/statusbar/src/lib/components/aiStatus.tsx
@@ -33,11 +33,11 @@ export default function AIStatus(props: AIStatusProps) {
   }, [props.plugin.isAiActive])
   return (
     <CustomTooltip
-      tooltipText={copilotActive ? "Remix Copilot activated" : "Remix Copilot disabled. To activate copilot, open a .sol file and toggle the ai switch at the top of the Ide"}
+      tooltipText={copilotActive ? "Copilot activated" : "Copilot disabled. To activate copilot, open a .sol file and toggle the ai switch at the top of the Ide"}
     >
       <div className="d-flex flex-row pr-2 text-white justify-content-center align-items-center">
         <span className={copilotActive === false ? "small mx-1 text-white semi-bold" : "small mx-1 text-white semi-bold" }>
-          {copilotActive === false ? 'Remix Copilot (disabled)' : 'Remix Copilot (enabled)'}
+          {copilotActive === false ? 'Copilot (disabled)' : 'Copilot (enabled)'}
         </span>
       </div>
     </CustomTooltip>
